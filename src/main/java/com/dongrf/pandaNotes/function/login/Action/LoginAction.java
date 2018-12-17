ed716a8063725f116c04c07d3b8877e83854dd25
mr_dongrf <mr_dongrf@163.com>
2018-12-17 22:54:45 +0800

    请求后台

--- src/main/java/com/dongrf/pandaNotes/function/login/Action/LoginAction.java
+++ src/main/java/com/dongrf/pandaNotes/function/login/Action/LoginAction.java
@@ -1,9 +1,17 @@
 package com.dongrf.pandaNotes.function.login.Action;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginAction {
 
-
+    @RequestMapping(value = "/doLogin",method = RequestMethod.GET)
+    public ModelAndView doLogin(@RequestParam int a){
+        System.out.println("The Param = " + a);
+        return null;
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
-<body>
-<h2>Hello World!</h2>
+<head>
+    <script>
+
+    </script>
+</head>
+<body onload="onloadf()">
+<h2>Hello World!112322121</h2>
+<a href="doLogin?a=15" target="_blank">点击打印数字</a>
 </body>
 </html>
